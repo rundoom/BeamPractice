dc737749baae6184d0431ed2f20b2e5f072b276d
Dmitry Borisov <rundoom1337@gmail.com>
2022-08-26 20:11:06 +0700

    Schema

--- src/main/java/org/practice/model/MeasurementEvent.java
+++ src/main/java/org/practice/model/MeasurementEvent.java
@@ -1,17 +1,21 @@
 package org.practice.model;
 
 import org.apache.beam.sdk.schemas.JavaBeanSchema;
+import org.apache.beam.sdk.schemas.JavaFieldSchema;
 import org.apache.beam.sdk.schemas.annotations.DefaultSchema;
+import org.apache.beam.sdk.schemas.annotations.SchemaFieldName;
 
 import java.io.Serializable;
 import java.text.DecimalFormat;
 
+@DefaultSchema(JavaFieldSchema.class)
 public class MeasurementEvent implements Serializable {
     private int timestamp;
     private int userId;
     private int location;
     private MeasurementType measurementType;
-    private double value;
+    @SchemaFieldName("value")
+    public double value;
 
     private DecimalFormat df = new DecimalFormat("####0.00");
 
@@ -22,6 +26,10 @@ public class MeasurementEvent implements Serializable {
         this.location = location;
         this.measurementType = measurementType;
         this.value = value;
+    }
+
+    public MeasurementEvent() {
+
     }
 
     public long getTimestamp() {
