fd9d96d0eafb16b8294930cbb337e35765c5c1bb
Dmitry Borisov <rundoom1337@gmail.com>
2022-08-26 20:12:19 +0700

    Removed tests from BeamManager

--- src/main/java/org/practice/beam/BeamManager.java
+++ src/main/java/org/practice/beam/BeamManager.java
@@ -1,16 +1,8 @@
 package org.practice.beam;
 
 import org.apache.beam.sdk.Pipeline;
-import org.apache.beam.sdk.coders.SerializableCoder;
-import org.apache.beam.sdk.io.TextIO;
 import org.apache.beam.sdk.options.PipelineOptions;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
-import org.apache.beam.sdk.transforms.Count;
-import org.apache.beam.sdk.transforms.Create;
-import org.apache.beam.sdk.transforms.Regex;
-import org.apache.beam.sdk.transforms.ToString;
-import org.practice.DataGenerator;
-import org.practice.model.MeasurementEvent;
 
 public class BeamManager {
     PipelineOptions options;
@@ -25,12 +17,7 @@ public class BeamManager {
         this(PipelineOptionsFactory.create());
     }
 
-    public void initBeam() {
-        DataGenerator generator = new DataGenerator();
-        pipeline.apply(Create.of(generator.generateNEvents(50)))
-                .apply(ToString.elements())
-//                .setCoder(SerializableCoder.of(MeasurementEvent.class))
-                .apply(TextIO.write().to("out").withSuffix(".txt"));
-        pipeline.run().waitUntilFinish();
+    public Pipeline getPipeline() {
+        return pipeline;
     }
 }
