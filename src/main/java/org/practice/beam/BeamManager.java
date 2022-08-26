1710704a16146c72e6e45c22c6d923184417ca8d
Dmitry Borisov <rundoom1337@gmail.com>
2022-08-26 16:25:09 +0700

    Beam init

---
+++ src/main/java/org/practice/beam/BeamManager.java
@@ -0,0 +1,22 @@
+package org.practice.beam;
+
+import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.options.PipelineOptions;
+import org.apache.beam.sdk.options.PipelineOptionsFactory;
+
+public class BeamManager {
+    PipelineOptions options;
+    Pipeline pipeline;
+
+    public BeamManager(PipelineOptions options) {
+        this.options = options;
+        this.pipeline = Pipeline.create(options);
+    }
+
+    public BeamManager() {
+        this(PipelineOptionsFactory.create());
+    }
+
+    public void initBeam() {
+    }
+}
