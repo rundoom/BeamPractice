f92f0f112b20e735a6962c9d4a017990e12c97b3
Dmitry Borisov <rundoom1337@gmail.com>
2022-09-02 17:40:39 +0700

    Naming

--- src/main/java/org/practice/demo/ConsumeKafkaSerializePostgres.java
+++ src/main/java/org/practice/demo/ConsumeKafkaSerializePostgres.java
@@ -4,10 +4,12 @@ import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.io.jdbc.JdbcIO;
 import org.apache.beam.sdk.io.kafka.KafkaIO;
 import org.apache.beam.sdk.transforms.Values;
+import org.apache.beam.sdk.transforms.WithTimestamps;
 import org.apache.beam.sdk.transforms.windowing.FixedWindows;
 import org.apache.beam.sdk.transforms.windowing.Window;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.joda.time.Duration;
+import org.joda.time.Instant;
 import org.practice.beam.BeamManager;
 import org.practice.model.MeasurementEvent;
 import org.practice.model.MeasurementEventDeserializer;
@@ -18,10 +20,10 @@ public class ConsumeKafkaSerializePostgres implements Serializable {
     transient BeamManager beamManager = new BeamManager();
 
     public static void main(String[] args) {
-        new ConsumeKafkaSerializePostgres().meanGlobal();
+        new ConsumeKafkaSerializePostgres().invoke();
     }
 
-    void meanGlobal() {
+    void invoke() {
         Pipeline pipeline = beamManager.getPipeline();
 
         pipeline
@@ -33,7 +35,7 @@ public class ConsumeKafkaSerializePostgres implements Serializable {
                         .withoutMetadata()
                 )
                 .apply(Values.create())
-//                .apply(WithTimestamps.of(kv -> Instant.ofEpochSecond(kv.timestamp)))
+                .apply(WithTimestamps.of(kv -> Instant.ofEpochSecond(kv.timestamp)))
                 .apply(Window.into(FixedWindows.of(Duration.standardMinutes(1))))
                 .apply(JdbcIO.<MeasurementEvent>write()
                         .withDataSourceConfiguration(JdbcIO.DataSourceConfiguration.create(
